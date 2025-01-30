0e59164b1870b76254e6f1338961a8e21c341eb8
DanielJDufour <daniel.j.dufour@gmail.com>
2025-01-30 14:20:14 -0500

    updated build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 rm -fr dist vfu.egg-info
 
 # make sure libraries used for publishing are up to date
-python3 -m pip install --user --upgrade setuptools wheel twine
+python3 -m pip install --upgrade setuptools wheel twine
 python3 -m pip install --upgrade twine
 
 python3 setup.py sdist
